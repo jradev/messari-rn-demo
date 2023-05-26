c75b63eecceacb201533885f92367b7361d0df79
Jomari Iringan <jom.iringan@gmail.com>
2023-05-26 19:42:46 +0800

    chore: initialize bare react native app

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  SafeAreaView, Text,
+} from 'react-native';
+
+
+function App(props){
+  return (
+    <SafeAreaView>
+    </SafeAreaView>
+  );
+}
+
+export default App;
