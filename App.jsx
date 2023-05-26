1f108d8419c7fb6353f2b0c85a4180ddea05386b
Jomari Iringan <jom.iringan@gmail.com>
2023-05-26 20:40:26 +0800

    fix: missing wrap provider

--- App.jsx
+++ App.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
+import { Provider } from 'react-redux';
+import AppNavigation from './src/navigation';
+import { store, persistor } from './src/redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import 'react-native-gesture-handler';
-import AppNavigation from './src/navigation';
 
 function App(props){
   return (
     <SafeAreaView style={{flex: 1}}>
-      <AppNavigation />
+      <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <AppNavigation />
+      </PersistGate>
+      </Provider>
     </SafeAreaView>
   );
 }
